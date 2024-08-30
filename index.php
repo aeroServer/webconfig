87dfdab805926f98189e1d5330d6d5b5d6c190cb
Yann <yann@cymdev.com>
2024-08-30 08:48:44 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$config_files = [
+    'Camera' => '~/.config/aeroServer-camera/parameters.json',
+];
+
+$config = [];
+
+foreach ($config_files as $title => $file) {
+    $config[$title] = json_decode($file);
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/pico.min.css">
+	<title>aeroServer Web Config</title>
+</head>
+<body>
+
+</body>
+</html>
